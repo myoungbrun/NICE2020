383acbd036b10e25c7c14c819c332bb46f72fc2d
myoungbrun <myoungbrun@gmail.com>
2025-08-11 10:49:59 +0200

    Update description for some variables

--- src/components/gross_economy.jl
+++ src/components/gross_economy.jl
@@ -4,7 +4,7 @@
 
 
 @defcomp grosseconomy begin
-    country = Index()                           #Note that a regional index is defined here
+    country = Index()                           # Index for countries
 
     I       = Parameter(index=[time, country]) 	# Investment (1e6 USD2017 per year)
     l       = Parameter(index=[time, country])  # Labor - population (thousands)
