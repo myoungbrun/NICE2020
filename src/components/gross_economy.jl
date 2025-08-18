94a913829258da5f82360e7573fd833c7ff9f42d
myoungbrun <myoungbrun@gmail.com>
2025-08-18 11:53:07 +0200

    Remove deprecated variables and update some comments

--- src/components/gross_economy.jl
+++ src/components/gross_economy.jl
@@ -18,7 +18,6 @@
     K       = Variable(index=[time, country])   # Capital (1e6 USD2017)
 
     function run_timestep(p, v, d, t)
-    # Note that the country dimension is defined in d and parameters and variables are indexed by 'c'
 
         # Define an equation for K
         for c in d.country
